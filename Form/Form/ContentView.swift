a21e201fdee90a70d229f0a9a43123fb25cd6ac8
SallyXie <zxc88633@gmail.com>
2022-08-07 17:08:23 +0800

    feat: 完成一個form畫面

--- Form/Form/ContentView.swift
+++ Form/Form/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    //🏴‍☠️🚩🏳️‍🌈🏳️‍⚧️🇺🇳🇹🇹🇹🇨
+    @State private var images = ["🏳️", "🏴", "🏴‍☠️", "🚩", "🏳️‍🌈", "🏳️‍⚧️", "🇺🇳", "🇹🇹", "🇹🇨"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("")) {
+                    HStack {
+                        Text("show preveiw")
+                        Spacer()
+                        NavigationLink(destination: Text("")) {
+                            Text("Always")
+                                .foregroundColor(Color.gray)
+                        }.fixedSize()
+                        
+                    }
+                }
+                
+                Section(header: Text("第二區區區區區區區區區區第二區區區區區區區區區區第二區區區區區區區區區區第二區區區區區區區區區區第二區區區區區區區區區區第二區區區區區區區區區區")
+                            .foregroundColor(Color.gray)
+                            .padding(10)
+                            .lineLimit(nil)) {
+                    HStack {
+                        Text("siri gogo")
+                        Spacer()
+                        NavigationLink(destination: Text("")) {
+                            Text("")
+                                .foregroundColor(Color.gray)
+                        }.fixedSize()
+                        
+                    }
+                }
+                
+                Section(header: VStack(alignment: .leading) {
+                    Text("第一區區").lineLimit(nil).padding(2)
+                    Text("第二區區").padding(2)
+                }) {
+                    ForEach(images, id: \.self) { img in
+                        HStack {
+                            Text("\(img)")
+                                .font(.largeTitle)
+                            Text("國旗")
+                        }
+                        
+                    }
+                }
+            }
+            .navigationBarTitle("Notification", displayMode: .inline)
+        }
     }
 }
 
