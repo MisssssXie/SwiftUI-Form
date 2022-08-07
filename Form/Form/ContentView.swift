c2250163297dfabd2dfc213809b51012a1b43cb1
SallyXie <zxc88633@gmail.com>
2022-08-07 16:44:30 +0800

    feat: add project

---
+++ Form/Form/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Form
+//
+//  Created by Sally Xie on 2022/8/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Form/Form/FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp.swift
+//  Form
+//
+//  Created by Sally Xie on 2022/8/7.
+//
+
+import SwiftUI
+
+@main
+struct FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
